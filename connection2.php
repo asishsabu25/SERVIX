34be6084d18a10515f62ab71af0062a16b1e2038
asishsabu25 <76831628+asishsabu25@users.noreply.github.com>
2022-02-10 20:04:41 +0530

    Add files via upload

--- connection2.php
+++ connection2.php
@@ -1,10 +1,14 @@
-<?php
 
+************MYSQL CONNECTION()******
+
+<div class="container" style="max-width: 1200px;">
+	<div class="row">
+<?php
 $firstname=$_POST['firstname'];
 $lastname=$_POST['lastname'];
 $gender=$_POST['gender'];
 
-$country=$_POST['country'];
+$state=$_POST['state'];
 $aadharno=$_POST['aadharno'];
 $job=$_POST['job'];
 $email=$_POST['email'];
@@ -12,22 +16,29 @@ $mobile=$_POST['mobile'];
 $address=$_POST['address'];
 $district=$_POST['district'];
 $pincode=$_POST['pincode'];
-$password=$_POST['password'];
-$confirmpassword=$_POST['confirmpassword'];
+$pass=$_POST['pass'];
+
 $conn = new mysqli('localhost','root','','test1');
 if($conn->connect_error){
 	die('connection failed: '.$conn->connect_error);
 
 }
 else{
-	$stmt = $conn->prepare("insert into registration(firstname,lastname,gender,country,aadharno,job,email,mobile,address,district,pincode,password,confirmpassword)values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
-	$stmt->bind_param("ssssissississ",$firstname,$lastname,$gender,$country,$aadharno,$job,$email,$mobile,$address,$district,$pincode,$password,$confirmpassword);
-	$stmt->execute();
-
-	echo "registration successfully..";
+	$stmt = $conn->prepare("INSERT INTO `registration`(`Firstname`, `Lastname`, `Gender`, `State`,`Email`, `Aadharno`, `Job`, `Mobile`, `Address`, `District`, `Pincode`, `Password`)values(?,?,?,?,?,?,?,?,?,?,?,?)");
+	$stmt->bind_param("ssssississss",$firstname,$lastname,$gender,$state,$email,$aadharno,$job,$mobile,$address,$district,$pincode,$pass);
+	
+    $stmt->execute();
+          echo "worker registration successfully..";
 	$stmt->close();
 	$conn->close();
+
 }
 
 
+
+
+
+
 ?>
+
+
