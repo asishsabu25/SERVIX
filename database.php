9f98ee05e83d1f684dfcd4b1370276bb32b21f43
asishsabu25 <76831628+asishsabu25@users.noreply.github.com>
2022-02-10 20:00:54 +0530

    Add files via upload

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $passdb = "";
-$db = "tutorpedia";
+$db = "test1";
 $connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
 
 ?>
--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php
 include( "database.php" );
 session_start();
-if(isset($_POST['login']))
+if(isset($_POST['submit']))
 {
-    $x = $_POST[ "username" ];
-$y = $_POST[ "pass" ];
+    $x = $_POST[ "email" ];
+$y = $_POST[ "password" ];
 
 
 $sql = "select * from login where username='$x ' and password='$y'";
 
 $result = mysqli_query( $connect, $sql );
 
-//echo "$x";
-//echo "$y";
+// echo "$x";
+// echo "$y";
 
 if ( $result->num_rows > 0 )
 {
@@ -30,17 +30,17 @@ if ( $result->num_rows > 0 )
         if ( $row["role"] == "user")
         
 
-           // header( 'Location:STUDENT/student/home.php' );
+            header( 'Location:customer/index.php' );
             #header( 'Location:test.php' );
-            header('Location:frontpage.html');
+            //header('Location:frontpage.html');
 
             else if ($row["role"]=="admin")
-            header('Location:frontpage.html');
-        	  // header('Location:ADMIN/admin/index.php');
+           // header('Location:frontpage.html');
+        	   header('Location:admin/index.php');
             
         else if ($row["role"]=="worker")
-        	   //header('Location:FACULTY/faculty/facultyindex.php');
-                header('Location:frontpage.html');
+        	   header('Location:worker/index.php');
+               // header('Location:frontpage.html');
         
        
          
@@ -52,7 +52,7 @@ if ( $result->num_rows > 0 )
 else {
 	//error message if SQL query fails
 	echo "<h3><span style='color:red; '>Invalid User ID & Password. Page Will redirect to Login Page after 3 seconds </span></h3>";
-	header( "refresh:3;url=index.html" );
+	header( "refresh:3;url=log.html" );
 }
   $connect->close();  
 
